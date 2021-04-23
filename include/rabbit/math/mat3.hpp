f95bb8eb369e1cc537f3dfba2c466c4613e2a2f9
demurzasty <thecyste@gmail.com>
2021-04-23 14:02:05 +0200

    fix mat3 identity creation

--- include/rabbit/math/mat3.hpp
+++ include/rabbit/math/mat3.hpp
@@ -5,7 +5,20 @@
 namespace rb {
     template<typename T>
     struct mat3 {
-        static constexpr mat4<T> identity() {
+        /**
+         * @brief Returns an instance of the identity matrix.
+         *
+         * Except for the main diagonal, whose elements are all ones,
+         * the identity matrix is a square matrix composed of zeros.
+         * Unlike all other matrix multiplications, the multiplication
+         * between an identity matrix and another matrix is commutative.
+         *
+         * This is useful for initializing a matrix to a known value
+         * before you perform additional operations, such as a transformation.
+         *
+         * @return Identity matrix.
+         */
+        static constexpr mat3<T> identity() {
             return {
                 1, 0, 0,
                 0, 1, 0,
