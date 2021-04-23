7cb38a4a83934204209b472c8276ca7a651f70af
demurzasty <thecyste@gmail.com>
2021-04-23 23:46:22 +0200

    cleanup

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -10,8 +10,6 @@ int main(int argc, char* argv[]) {
     window_manager window_manager;
     auto window = window_manager.create({});
 
-    window->size();
-
     graphics_device_manager graphics_device_manager;
     auto graphics_device = graphics_device_manager.create({ window });
 
