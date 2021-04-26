ad77c386cfa73ace063f1834397e56dfbe389618
demurzasty <thecyste@gmail.com>
2021-04-26 14:11:29 +0200

    add asset manager

--- include/rabbit/asset/asset_manager.hpp
+++ include/rabbit/asset/asset_manager.hpp
@@ -1,11 +1,39 @@
 #pragma once
 
-#include "../core/type_info.hpp"
+#include "fwd.hpp"
+#include "../core/fwd.hpp"
+#include "../core/injector.hpp"
 
+#include <string>
 #include <memory>
 #include <unordered_map>
 
-namespace rb {
+// TODO: Identify asset by unique id.
 
+namespace rb {
+    class asset_manager {
+    public:
+        asset_manager(injector& injector);
+
+        template<typename Asset, typename Loader>
+        void add_loader() {
+            _loaders.emplace(type_id<Asset>().hash(), _injector.resolve<Loader>());
+        }
+
+        template<typename Asset>
+        std::shared_ptr<Asset> load(const std::string& filename) {
+            return std::static_pointer_cast<Asset>(_load(type_id<Asset>().hash(), filename));
+        }
+
+    private:
+        std::shared_ptr<void> _load(id_type asset_id, const std::string& filename);
+
+        json _load_metadata(const std::string& filename) const;
+
+    private:
+        injector& _injector;
+        std::unordered_map<id_type, std::shared_ptr<loader>> _loaders;
+        std::unordered_map<std::string, std::weak_ptr<void>> _assets;
+    };
 }
 
---
+++ src/asset/asset_manager.cpp
@@ -0,0 +1,35 @@
+#include <rabbit/asset/asset_manager.hpp>
+#include <rabbit/asset/loader.hpp>
+#include <rabbit/core/json.hpp>
+
+#include <fstream>
+
+using namespace rb;
+
+asset_manager::asset_manager(injector& injector)
+    : _injector(injector) {
+}
+
+std::shared_ptr<void> asset_manager::_load(id_type asset_id, const std::string& filename) {
+    auto& asset = _assets[filename];
+    if (!asset.expired()) {
+        return asset.lock();
+    }
+
+    auto& loader = _loaders.at(asset_id);
+    auto loaded_asset = loader->load(filename, _load_metadata(filename));
+    asset = loaded_asset;
+    return loaded_asset;
+}
+
+json asset_manager::_load_metadata(const std::string& filename) const {
+    json metadata;
+
+    std::ifstream stream{ filename + ".meta", std::ios::in };
+    if (stream.is_open()) {
+        stream >> metadata;
+    }
+
+    return metadata;
+}
+
