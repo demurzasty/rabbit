5eaa84140f8e8c6e462f430cd851757ba53a0add
demurzasty <thecyste@gmail.com>
2021-04-26 13:21:09 +0200

    add json

--- include/rabbit/core/fwd.hpp
+++ include/rabbit/core/fwd.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <nlohmann/json_fwd.hpp>
+
 namespace rb {
     class injector;
+    using nlohmann::json;
     struct version;
 }
---
+++ include/rabbit/core/json.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "fwd.hpp"
+
+#include <nlohmann/json.hpp>
