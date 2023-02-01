722eb8395b9c803bdf0ffb4e62fbef4a6f7be24d
demurzasty <thecyste@gmail.com>
2023-02-01 23:33:42 +0100

    Add texture source file.

--- src/graphics/texture.cpp
+++ src/graphics/texture.cpp
@@ -1,3 +1,19 @@
 #include <rabbit/graphics/texture.hpp>
 
 using namespace rb;
+
+texture::texture(renderer& renderer)
+    : m_renderer(renderer), m_id(renderer.create_texture()) {
+}
+
+texture::~texture() {
+    m_renderer.destroy_texture(m_id);
+}
+
+uvec2 texture::size() const {
+    return m_renderer.get_texture_size(m_id);
+}
+
+pixel_format texture::format() const {
+    return m_renderer.get_texture_format(m_id);
+}
